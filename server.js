9655f2991b557842508a35b43d53a411e6816478
WillOow <theotimefd@aol.com>
2019-06-11 10:34:50 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -9,10 +9,13 @@ function getRandomInt(max) {return Math.floor(Math.random() * Math.floor(max));}
 var tour = 1; //tour
 var idDuTour;
 
-var counterVal = 10;
+var counterVal = 60;
+var nombre = 1;
+var tours = 1;
  
 users = []; //liste des noms(couleurs)
 vies = []; // dans le meme ordre que les sockets
+boucliers = [];
 connections = []; // liste des sockets connectés avec un id et un nom (couleur)
 var couleursNonDispo = [];
 var premierJoueur;
@@ -46,15 +49,20 @@ app.get("/", function(req, res){
 
 
 io.sockets.on("connection", function(socket){
+
+  
+
   connections.push(socket);
   console.log("Connecté : %s sockets connectés", connections.lenght);
+
   var i;
   for (i in couleursNonDispo) {
     io.sockets.emit("enleverCouleur", couleursNonDispo[i]);
   }
+
   // CONNEXION ET DECONNEXION
   socket.on("disconnect", function(data){
-      // déconnexion
+      // disconection
     users.splice(users.indexOf(socket.username), 1);
     updateUsernames();
     connections.splice(connections.indexOf(socket), 1);
@@ -72,6 +80,8 @@ io.sockets.on("connection", function(socket){
     
     users.push(socket.username);
     vies.push(100);
+    boucliers.push(0);
+    
     updateUsernames();
 
     couleursNonDispo.push(data);
@@ -83,6 +93,7 @@ io.sockets.on("connection", function(socket){
   function updateUsernames(){
     io.sockets.emit("get users", users);
   }
+
   // ----------- LOGIQUE DU JEU -----------
   //    --> tour
   socket.on("finTour", function(data) {
@@ -95,7 +106,26 @@ io.sockets.on("connection", function(socket){
           idDuTour = connections[i+1].id;
         } else { 
           idDuTour = connections[0].id; 
-          io.sockets.emit("tour", 0); //quand un tour est passé (tous les joueurs ont joué une fois)
+          tours ++;
+          nombre++;
+          if (nombre == 5) { // un coffre tous les 5 tours
+        
+            if (tours < 20) {
+              var entier2 = Math.ceil(Math.random()*25) ;
+              var entier1 = Math.ceil(Math.random()*25) ;
+              var alert = "Un coffre venant du ciel a atterri sur le plateau !" + "\n" + "Veuillez le poser aux coordonnées suivantes :" + "\n" + "x:" + entier1 + "\n" + "y:" + entier2;
+            }
+            if (tours>=20) {
+              var entier2 = Math.ceil(Math.random()*25) ;
+              var entier1 = Math.ceil(Math.random()*25) ;
+              var entier3 = Math.ceil(Math.random()*25) ;
+              var entier4 = Math.ceil(Math.random()*25) ;
+              var alert = "Deux coffres venant du ciel ont atterri sur le plateau !" + "\n" + "Veuillez les poser aux coordonnées suivantes :" + "\nCoffre 1 :\n" + "x:" + entier1 + " y:" + entier2 + "\nCoffre 2 :\n" + "x:" + entier3 + " y:" + entier4;
+              
+            }
+          nombre = 0;
+          }
+          io.sockets.emit("tour", alert); //quand un tour est passé (tous les joueurs ont joué une fois)
         }
       }
     }
@@ -111,10 +141,23 @@ io.sockets.on("connection", function(socket){
     //le client envoie la couleur a attaquer, il faut la convertir en id
     var id = couleurToid(data[0]);
     var pos = idToPlace(id);
-    vies[pos] -= data[1];
+    var vieapres = data[1];
+    while (boucliers[pos] > 0 && vieapres > 0) {
+      vieapres -= 1;
+      boucliers[pos] -= 1;
+    }
+    vies[pos] -= vieapres;
     if (vies[pos]<=0) { vies[pos] = 0; }
-    io.to(id).emit("attaqué", vies[pos]);
+    io.to(id).emit("attaqué", [vies[pos], boucliers[pos]]);
   });
+
+  socket.on("armure", function(data) {
+    var id = couleurToid(data[0]);
+    var pos = idToPlace(id);
+    boucliers[pos] = data[1];
+  });
+
+  
   socket.on("lobby" , function() {
     if (users.length == 1) {
       var counter = setInterval(function() {
@@ -186,9 +229,12 @@ io.sockets.on("connection", function(socket){
 
 });
 
+
+
 function choisirJoueurDebut() {
   premierJoueur = connections[0].id; //length TH
   //premierJoueur = connections[0].id;
   io.sockets.emit("Tour", false);
   io.to(premierJoueur).emit("Tour", true);
+  console.log("choisirJoueurDebut");
 }
